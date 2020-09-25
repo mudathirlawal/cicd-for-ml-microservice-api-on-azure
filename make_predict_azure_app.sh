5330e77664e5979f9bea1f668a0c1b047dd01557
Mudathir Lawal <nigercode@gmail.com>
2020-09-25 14:33:58 +0000

    feat: Configure app URL and port

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
-     #your application name <yourappname>goes here
+     -X POST https://app.azurewebsites.net:$PORT/predict 
